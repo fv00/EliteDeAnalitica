dcb23493c6acd6d1e05e7dbfd9fa853b825cb7cd
RNHATER <sfrancov@unal.edu.co>
2022-01-10 12:37:34 -0500

    Adelantos

--- Ejercicio4.R
+++ Ejercicio4.R
@@ -1,32 +1,61 @@
+#Lectura de librerías
 library(ISLR)
+library(corrplot)
+library(gam)
+
+#Lectura de datos
 data(Credit)
 
-#https://cran.r-project.org/web/packages/ISLR/ISLR.pdf
-Credit <- subset(Credit, select = -c(ID))
+#Validacion cruzada
+set.seed(123)
+size = ceiling(nrow(Credit)*0.8)
+training = sample(1:nrow(Credit), size)
+test = which(!1:nrow(Credit) %in% training)
 
-attach(Credit)
+#Variable respuesta:
+y_train <- training$Balance
+y_train <- scale(y_train)
 
-# Definicion Balance:
-#--The balance in your bank account is the amount of money you have in it.
-#--Cantidad de dinero que tiene una persona en su cuenta bancaria
+#Distribucion variable respuesta:
+hist(y_train)
 
-## Preprocesamiento basico
-Cards <- as.factor(Cards)
-Education <- as.factor(Education)
+#Grafica de correlacion:
+cor = cor(training[c(2:6, 12)])
+corrplot(cor)
 
-## Varibles categoricas
-#plot(Gender, Balance)
-plot(Student, Balance) # Se observa desbalanceo en los datos
-#plot(Married, Balance)
-#plot(Ethnicity, Balance)
-#plot(Education, Balance)
 
-barplot(Cards)
-barplot(Education)
+#Diagramas de dispersion:
+pairs(training[c(2:6, 12)])
 
-## Variables numericas
-plot(Income, Balance)
-plot(Limit, Balance) # Relacion obvia
-plot(Rating, Balance)
-plot(Age)
+#Boxplots:
+par(mfrow = c(2,2))
 
+# boxplot(training$Balance ~ training$Gender)
+boxplot(training$Balance ~ training$Education)
+boxplot(training$Balance ~ training$Married)
+boxplot(training$Balance ~ training$Ethnicity)
+boxplot(training$Balance ~ training$Student)
+
+#Modelamiento:
+#Balance en base a las covariables sin usar splines
+mod_gam1 <- gam(Balance ~ Limit + Rating +  Student,
+                family = Gamma(link = "identity"),
+                data = Credit,
+                subset = training)
+#Balance en base a las covariables usasndo splines en una covariable
+mod_gam2 <- gam(Balance ~ s(Limit) + Rating + Student,
+                family = Gamma(link = "identity"),
+                data = Credit,
+                subset = training)
+#Balance en base a las covariables usando loess
+mod_gam3 <- gam(Balance ~ limit + lo(Rating),
+                family = Gamma(link = "identity"),
+                data = Credit,
+                subset = training)
+#Balance en base a las covariables usando loess y splines
+mod_gam4 <- gam(Balance ~ s(Limit) + lo(Rating),
+                family = Gamma(link = "identity"),
+                data = Credit,
+                subset = training)
+
+#Familia de modelos gamma
