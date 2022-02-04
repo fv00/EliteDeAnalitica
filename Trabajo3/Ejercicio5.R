631ffa7f9939931365ec9fb2ffecd22cace45815
RNHATER <sfrancov@unal.edu.co>
2022-02-04 18:20:00 -0500

    Update Ejercicio5.R

--- Trabajo3/Ejercicio5.R
+++ Trabajo3/Ejercicio5.R
@@ -2,13 +2,24 @@
 data(USArrests)
 
 #Agrupamiento de datos
+
 distancias_euclideanas <- dist(USArrests, method = 'euclidean')
 clusters_completos <- hclust(distancias_euclideanas, method = 'complete')
 
 plot(clusters_completos)
 
+cortes_completos <- cutree(clusters_completos, k=3)
+##Estados pertenecientes al grupo1:
+cortes_completos[cortes_completos==1]
+
+##Estados pertenecientes al grupo2:
+cortes_completos[cortes_completos==2]
+
+##Estados pertenecientes al grupo3:
+cortes_completos[cortes_completos==3]
+
 #Escalamiento de datos:
-USArrests_escalado <- scale(USArrests, center = TRUE, scale = TRUE)
+USArrests_escalado <- scale(USArrests, center = FALSE, scale = TRUE)
 
 #Agrupamiento de datos:
 distancias_escaladas <- dist(USArrests_escalado, method = 'euclidean')
